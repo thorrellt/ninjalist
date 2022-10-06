4b98ac93906b432b9f92695696ca801366c1dd0b
ThorrellT <thorrellt@gmail.com>
2022-10-06 12:01:50 -0400

    Lesson 2 finish

---
+++ pages/about.js
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt
+        consequatur nemo sapiente assumenda illo reprehenderit explicabo quasi
+        vitae adipisci nostrum, modi consequuntur ut maiores, labore, eos nihil
+        distinctio cum!
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius magnam
+        tenetur accusantium, doloremque at ab delectus, atque molestias maiores
+        culpa cupiditate. Ipsum distinctio perferendis perspiciatis dignissimos
+        autem laboriosam dicta sequi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
+
+// http://192.168.0.178:3000/
